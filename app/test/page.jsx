281a643289b70abc13d4ae73ef6addf324b93fbc
adis-tek <67308354+adis-tek@users.noreply.github.com>
2024-11-15 19:48:36 -0600

    testing

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -7,18 +7,23 @@ export default function test() {
     <div>
       <h1>Test Page</h1>
       <Canvas
-      // camera={{
-      //   position: [0, 0, 5],
-      //   fov: 75,
-      //   aspect: window.innerWidth / window.innerHeight,
-      //   near: 0.1,
-      //   far: 1000,
-      // }}
+        // camera={{
+        //   position: [0, 0, 5],
+        //   fov: 75,
+        //   aspect: window.innerWidth / window.innerHeight,
+        //   near: 0.1,
+        //   far: 1000,
+        // }}
+        camera={{ position: [0, 0, 5] }}
       >
         <ambientLight intensity={1.5} />
         <directionalLight position={[0, 0, 5]} color='red' />
-        <mesh>
+        <mesh postion={2}>
           <boxGeometry args={[2, 2, 2]} />
+          <meshPhongMaterial color='light-orange' />
+        </mesh>
+        <mesh postion={-5}>
+          <sphereGeometry args={[2, 2, 2]} />
           <meshPhongMaterial color='light-orange' />
         </mesh>
       </Canvas>
