8fbe415580d41cf28bb6d3276e9619725006e809
adis-tek <67308354+adis-tek@users.noreply.github.com>
2024-11-27 21:25:44 -0600

    test adjustment

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,12 +1,13 @@
 'use client'
 
 import { Canvas } from '@react-three/fiber'
+import { useFrame } from '@react-three/fiber'
 
 export default function test() {
   return (
     <div>
-      <h1>Test Page</h1>
-      <Canvas
+      {/* <h1>Test Page</h1> */}
+      {/* <Canvas
         // camera={{
         //   position: [0, 0, 5],
         //   fov: 75,
@@ -14,16 +15,32 @@ export default function test() {
         //   near: 0.1,
         //   far: 1000,
         // }}
-        camera={{ position: [0, 0, 5] }}
+        camera={{ position: [5, 1.5, 3] }}
       >
         <ambientLight intensity={1.5} />
-        <directionalLight position={[0, 0, 5]} color='red' />
-        <mesh postion={2}>
+        <directionalLight position={[0, 0, 5]} color='white' />
+        <group>
+          <mesh postion={[9, 0, 0]} rotation-x={Math.PI * 1} scale={[1.5, 1.5, 1.5]}>
+            <boxGeometry args={[2, 2, 2]} />
+            <meshPhongMaterial color='light-orange' />
+          </mesh>
+        </group>
+      </Canvas> */}
+      <Canvas
+        style={{ height: '100vh' }}
+        // camera={{
+        //   position: [0, 0, 5],
+        //   fov: 75,
+        //   aspect: window.innerWidth / window.innerHeight,
+        //   near: 0.1,
+        //   far: 1000,
+        // }}
+        camera={{ position: [9, 1.5, 3] }}
+      >
+        <ambientLight intensity={1.5} />
+        <directionalLight position={[0, 0, 5]} color='white' />
+        <mesh postion={[9, 0, 0]} rotation-x={Math.PI * 1} scale={[1.5, 1.5, 1.5]}>
           <boxGeometry args={[2, 2, 2]} />
-          <meshPhongMaterial color='light-orange' />
-        </mesh>
-        <mesh postion={-5}>
-          <sphereGeometry args={[2, 2, 2]} />
           <meshPhongMaterial color='light-orange' />
         </mesh>
       </Canvas>
