95bcb885d06487134bf5c63925f43b24c18f89e9
adis-tek <67308354+adis-tek@users.noreply.github.com>
2024-11-30 20:16:44 -0600

    orbital controls

--- src/components/canvas/cube.jsx
+++ src/components/canvas/cube.jsx
@@ -3,8 +3,11 @@
 import { useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
 import { useMemo, useRef, useState } from 'react'
+import { OrbitControls } from '@react-three/drei'
+import { useThree, extend } from '@react-three/fiber'
 
 export default function Cube() {
+  const { camera, gl } = useThree()
   const cubeRef = useRef()
   useFrame((state, delta) => {
     const t = state.clock.getElapsedTime()
@@ -12,6 +15,7 @@ export default function Cube() {
   })
   return (
     <>
+      <OrbitControls />
       <mesh ref={cubeRef} postion={[0, 0, 0]} rotation-x={Math.PI * 1} scale={[1.5, 1.5, 1.5]}>
         <boxGeometry args={[2, 2, 2]} />
         <meshPhongMaterial color='light-orange' />
