689aee81e729846a42141af10f06ed39063065fe
adis-tek <67308354+adis-tek@users.noreply.github.com>
2024-12-01 23:30:17 -0600

    drei

--- src/components/canvas/cube.jsx
+++ src/components/canvas/cube.jsx
@@ -3,7 +3,7 @@
 import { useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
 import { useMemo, useRef, useState } from 'react'
-import { OrbitControls } from '@react-three/drei'
+import { Html, MeshReflectorMaterial, TransformControls, Text, OrbitControls, PivotControls } from '@react-three/drei'
 import { useThree, extend } from '@react-three/fiber'
 
 export default function Cube() {
@@ -16,10 +16,23 @@ export default function Cube() {
   return (
     <>
       <OrbitControls />
-      <mesh ref={cubeRef} postion={[0, 0, 0]} rotation-x={Math.PI * 1} scale={[1.5, 1.5, 1.5]}>
+      <TransformControls object={cubeRef} />
+      {/* <mesh ref={cubeRef} postion={[0, 0, 0]} rotation-x={Math.PI * 1} scale={[1.5, 1.5, 1.5]}>
         <boxGeometry args={[2, 2, 2]} />
         <meshPhongMaterial color='light-orange' />
-      </mesh>
+      </mesh> */}
+      <PivotControls position={[0, 0, 0]} scale={[1.5, 1.5, 1.5]} rotation-x={Math.PI * 1}>
+        <mesh ref={cubeRef} postion={[0, 0, 0]} rotation-x={Math.PI * 1} scale={[1.5, 1.5, 1.5]}>
+          <boxGeometry args={[2, 2, 2]} />
+          <meshPhongMaterial color='light-orange' />
+          <Html wrapperClass='html-wrapper' center>
+            My html
+          </Html>
+          <MeshReflectorMaterial />
+        </mesh>
+
+        <Text>My Text</Text>
+      </PivotControls>
     </>
   )
 }
