ed027b89a437215880e0d87b2500ae4bedab13c6
adis-tek <67308354+adis-tek@users.noreply.github.com>
2024-12-03 22:31:24 -0600

    on initiate staging state

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -5,6 +5,10 @@ import { useFrame } from '@react-three/fiber'
 import Cube from '../../src/components/canvas/cube'
 
 export default function test() {
+  const created = ({ gl }) => {
+    console.log('created', gl)
+    // gl.setClearColor('#ff0000', 1)
+  }
   return (
     <div>
       {/* <h1>Test Page</h1> */}
@@ -36,10 +40,18 @@ export default function test() {
         //   near: 0.1,
         //   far: 1000,
         // }}
-        camera={{ position: [9, 1.5, 3] }}
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 200,
+          position: [-4, 3, 6],
+        }}
+        onCreated={created}
       >
+        <color args={['beige']} attach='background' />
         <ambientLight intensity={1.5} />
         <directionalLight position={[0, 0, 5]} color='white' />
+
         <Cube />
       </Canvas>
       <Canvas
